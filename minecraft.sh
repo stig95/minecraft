57a8c7ef5e0f8c2702e0979c12d1bc226aca542a
Stig Kristian Rothaug <16879353+stig95@users.noreply.github.com>
2021-09-09 23:32:19 +0200

    testing

--- minecraft.sh
+++ minecraft.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install;curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre
-adduser mcserver
+sudo apt install;curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre pwgen
+useradd -m mcserver
 sudo su - mcserver
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
-yes|./mcserver install
+./mcserver auto-install
 ./mcserver start
