aad57dcd0c11ef4af62ee7781c21270d2f5955b7
Stig Kristian Rothaug <16879353+stig95@users.noreply.github.com>
2021-09-09 22:47:54 +0200

    Changed sudo right

--- minecraft.sh
+++ minecraft.sh
@@ -3,6 +3,6 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install;curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre
 adduser mcserver
-su - mcserver
+sudo su - mcserver
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
 yes|./mcserver install
