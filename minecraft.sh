90666452cafbefb1714446026c9750146dc4c022
Stig Kristian Rothaug <16879353+stig95@users.noreply.github.com>
2021-09-09 23:42:55 +0200

    Removed semi-colon

--- minecraft.sh
+++ minecraft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install;curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre pwgen
+sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre pwgen
 sudo useradd -m mcserver
 sudo su - mcserver
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
