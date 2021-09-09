7697cc057e246cfac21787a10c1666d0bbcc9ec4
Stig Kristian Rothaug <16879353+stig95@users.noreply.github.com>
2021-09-09 23:38:27 +0200

    Added sudo

--- minecraft.sh
+++ minecraft.sh
@@ -2,7 +2,7 @@
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install;curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre pwgen
-useradd -m mcserver
+sudo useradd -m mcserver
 sudo su - mcserver
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
 ./mcserver auto-install
