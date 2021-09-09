c9cb2c6ab36ded81f623ea7e5b8c7968f9487563
Stig Kristian Rothaug <16879353+stig95@users.noreply.github.com>
2021-09-09 22:42:37 +0200

    Fixed bash

--- minecraft.sh
+++ minecraft.sh
@@ -1,4 +1,4 @@
-#!/sh
+#!/bin/sh
 sudo dpkg --add-architecture i386;
 sudo apt update; 
 sudo apt install;curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat openjdk-16-jre &&
